279812f2011e415ca5117f9cd08a9f21469f26f7
Paolo Davila <pdavila@iesebre.com>
2016-05-12 14:43:25 +0200

    Create tables and Seeders

--- database/migrations/2016_05_06_143913_create_people_table.php
+++ database/migrations/2016_05_06_143913_create_people_table.php
@@ -18,17 +18,24 @@ class CreatePeopleTable extends Migration
             $table->string('sn1');
             $table->string('sn2');
             $table->string('email')->unique();
+            $table->string('secondary_email');
             $table->string('official_id')->unique();
+            $table->date('date_of_birth');
+            $table->string('homePostalAddress');
+            $table->string('locality_name');
             $table->string('mobile');
+            $table->string('photo');
             $table->timestamps();
 
             $table->integer('user_id')->unsigned();
+            $table->integer('locality_id')->unsigned();
 
         });
 
-//        Schema::table('people', function ($table) {
-//            $table->foreign('user_id')->references('id')->on('users');
-//        });
+        Schema::table('people', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            //$table->foreign('locality_id')->references('id')->on('locality');
+        });
     }
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\People;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
@@ -18,23 +19,49 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $faker = Faker\Factory::create('es_ES');
+        $this->seedUser($faker);
         $this->seedPeople($faker);
+
 
         Model::reguard();
     }
 
+    public function seedUser($faker) {
+        foreach(range(1,20) as $number) {
+            $user = new User();
+
+            $user->username = $faker->username;
+            $user->email = $faker->freeEmail;
+            $user->password = $faker->password;
+
+            $user->save();
+        }
+    }
+
     public function seedPeople($faker) {
-        foreach(range(0,20) as $number) {
+
+        $users = User::all()->lists('id')->toArray();
+
+        foreach(range(1,20) as $number) {
             $people = new People();
 
-            $people->givenName = $faker->lastName;
-            $people->sn1 = $faker->firstNameMale;
-            $people->sn2 = $faker->firstNameFemale;
-            $people->email = $faker->email;
+            $people->givenName = $faker->firstName;
+            $people->sn1 = $faker->lastName;
+            $people->sn2 = $faker->lastName;
+            $people->email = $faker->freeEmail;
+            $people->secondary_email = $faker->safeEmail;
             $people->official_id = $faker->dni;
+            $people->date_of_birth = $faker->date($format = 'Y-m-d', $max = 'now');
+            $people->homePostalAddress = $faker->streetAddress;
+            $people->locality_name = $faker->city;
             $people->mobile = $faker->phoneNumber;
+            $people->photo = $faker->imageUrl($width = 640, $height = 480, 'people');
+
+            $people->user_id = $faker->randomElement($users);
 
             $people->save();
         }
     }
+
+
 }
