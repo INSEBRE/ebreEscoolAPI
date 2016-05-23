73312eb60bb2bbdd05f42fcb120f74ea8d16fe94
Paolo Davila <pdavila1313@gmail.com>
2016-05-23 12:54:10 +0200

    Added group in Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,10 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('user', 'UserController',
-    ['only' => ['index', 'store', 'update', 'destroy', 'show']]
-);
+Route::group(['prefix' => 'api/'], function () {
+    Route::resource('user', 'UserController',
+        ['only' => ['index', 'store', 'update', 'destroy', 'show']]
+    );
 
-Route::resource('people', 'PeopleController',
-    ['only' => ['index', 'store', 'update', 'destroy', 'show']]
-);
+    Route::resource('people', 'PeopleController',
+        ['only' => ['index', 'store', 'update', 'destroy', 'show']]
+    );
+});
